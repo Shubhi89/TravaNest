e096dd8375fdedb57f0e935d7c8bbf66846cf982
Shubhi Dwivedi <shubhidwivedi1415@gmail.com>
2025-05-27 11:19:08 +0530

    post login page

--- routes/user.js
+++ routes/user.js
@@ -1,33 +1,60 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
-const passport = require('passport');
+const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
-router.get('/signup', (req, res) => {
-    res.render('users/signup.ejs');
+router.get("/signup", (req, res) => {
+  res.render("users/signup.ejs");
 });
 
-router.post("/signup", wrapAsync(async (req, res, next) => {
-    try{
-        let { username, email, password } = req.body;
-        const newUser = new User({ username, email });
-        const registeredUser = await User.register(newUser, password); 
+router.post(
+  "/signup",
+  wrapAsync(async (req, res, next) => {
+    try {
+      let { username, email, password } = req.body;
+      const newUser = new User({ username, email });
+      const registeredUser = await User.register(newUser, password);
+      req.login(registeredUser, (error) => {
+        if (error) {
+          return next(error);
+        }
         req.flash("success", "Welcome to Travanest!");
         res.redirect("/listings");
+      });
     } catch (error) {
-        req.flash("error", error.message);
-        res.redirect("/signup");
+      req.flash("error", error.message);
+      res.redirect("/signup");
     }
-}));
+  })
+);
 
-router.get('/login', (req, res) => {
-    res.render('users/login.ejs');
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
 });
 
-router.post("/login" ,passport.authenticate("local" , { failureRedirect: "/login", failureFlash: true }) , async (req, res) => {
+router.post(
+  "/login",
+  saveRedirectUrl,
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
     req.flash("success", "Welcome back to TravaNest!");
+    res.redirect(res.locals.redirectUrl || "/listings");
+  }
+);
+
+router.get("/logout", (req, res, next) => {
+  req.logout((error) => {
+    if (error) {
+      return next(error);
+    }
+    req.flash("success", "Logged out successfully!");
     res.redirect("/listings");
+  });
 });
 
 module.exports = router;
