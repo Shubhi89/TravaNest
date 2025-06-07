dc2a4c74fba8717c5e1adc10ed747d7aad40578a
Shubhi Dwivedi <shubhidwivedi1415@gmail.com>
2025-06-07 10:25:00 +0530

    adding image preview

--- controllers/listings.js
+++ controllers/listings.js
@@ -44,7 +44,9 @@ module.exports.editListing = async (req, res) => {
       req.flash("error", "Listing not found");
       return res.redirect("/listings");
     }
-    res.render("listings/edit.ejs", { listing });
+    let originalImageUrl = listing.image.url;
+    originalImageUrl = originalImageUrl.replace("/upload" , "/upload/w_250");
+    res.render("listings/edit.ejs", { listing , originalImageUrl });
 };
 
 module.exports.updateListing = async (req, res) => {
